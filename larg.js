87eb12fed83d13fc95f566d7e15a5af0f74c7b89
Adam <33525058+Cyber28@users.noreply.github.com>
2019-02-19 16:58:10 +0100

    Fixed major bug

--- larg.js
+++ larg.js
@@ -5,25 +5,25 @@
  */
 module.exports = (args) => {
   let p = {};
-  let l = [];
+  let i = [];
 
   const rHyphens    = (v) => v.replace(/^\-+/g, '');
   const cApplicable = (v) => ( isNaN(v) ? (v.toString().toLowerCase() === 'true' ? true : (v.toString().toLowerCase() === 'false' ? false : v)) : Number(v));
 
   for (let _ = 0; _ < args.length; _++) {
-    const e = args[i].indexOf('=');
-    const r = args[i].charAt(0) === '-' && args.length - 1 >= i + 1 && args[_ + 1].indexOf('=') === -1 && args[_ + 1].charAt(0) !== '-';
+    const e = args[_].indexOf('=');
+    const r = args[_].charAt(0) === '-' && args.length - 1 >= _ + 1 && args[_ + 1].indexOf('=') === -1 && args[_ + 1].charAt(0) !== '-';
     const n = e === -1 ? rHyphens(args[_]) : rHyphens(args[_].slice(0, e));
 
-    if ( e !== -1 ) p[ n ] = cApplicable( args[ i ].slice(e + 1));
+    if ( e !== -1 ) p[ n ] = cApplicable( args[ _ ].slice(e + 1));
     else if (r) {
       p[n] = cApplicable(args[_ + 1] );
       _++;
     } else if (args[_].charAt(0) === '-') {
       if (args[_].charAt(1) === '-' ) p[n] = true;
       else for (let b = 0; b < n.length; b++) p[n.charAt(b)] = true;
-    } else l.push(cApplicable(n));
+    } else i.push(cApplicable(n));
   }
 
-  return Object.assign(p, { '_': l });
+  return Object.assign(p, { '_': i });
 };
